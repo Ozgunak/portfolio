ced7c92789b65954b977ac292547e4542b7aa7f2
Ozgun Aksoy <zgnaksoy@gmail.com>
2023-10-01 15:34:08 -0700

    Initial Commit

---
+++ portfolio/portfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  portfolioApp.swift
+//  portfolio
+//
+//  Created by özgün aksoy on 2023-10-01.
+//
+
+import SwiftUI
+
+@main
+struct portfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
