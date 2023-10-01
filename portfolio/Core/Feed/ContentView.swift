7dee4e76128637528971d2e11ac3344824fbd3ff
Ozgun Aksoy <zgnaksoy@gmail.com>
2023-10-01 16:36:56 -0700

    add feed view

---
+++ portfolio/Core/Feed/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  portfolio
+//
+//  Created by özgün aksoy on 2023-10-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isLoading: Bool = false
+
+    var body: some View {
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                if isLoading {
+                    ProgressView()
+                        .frame(width: 100, height: 100)
+                }
+                LazyVStack(spacing: 20) {
+                    ForEach(0...20, id: \.self) { index in
+                        FeedItemView()                        
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image("titleimage")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+//                        AllMessagesView()
+//                            .navigationBarBackButtonHidden()
+                    } label: {
+                        Image(systemName: "plus.bubble")
+                            .resizable()
+                            .scaledToFill()
+                    }
+                }
+            }
+            .refreshable {
+//                try? await viewModel.fetchPosts()
+            }
+            .task {
+//                do {
+//                    isLoading = true
+//                    try await viewModel.fetchPosts()
+//                    isLoading = false
+//                } catch {
+//                    print("Error: error fetching posts \(error.localizedDescription)")
+//                }
+            }
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
