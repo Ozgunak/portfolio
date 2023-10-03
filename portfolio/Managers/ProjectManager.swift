3311be02dfdfc12e71f03bc5367d6c30145e8363
Ozgun Aksoy <zgnaksoy@gmail.com>
2023-10-03 12:12:37 -0700

    add project manager

---
+++ portfolio/Managers/ProjectManager.swift
@@ -0,0 +1,37 @@
+//
+//  ProjectManager.swift
+//  portfolio
+//
+//  Created by özgün aksoy on 2023-10-03.
+//
+
+import Foundation
+import Firebase
+
+struct ProjectManager {
+    static let shared = ProjectManager()
+    
+    private init() {}
+    
+    private let projectsCollectionPath = Firestore.firestore().collection(FirestorePath.projects.rawValue)
+    
+    func fetchHomeFeedProjects() async throws -> [Project] {
+        let snapshot = try await projectsCollectionPath.order(by: "timeStamp", descending: true).getDocuments()
+        var projects = snapshot.documents.compactMap( { try? $0.data(as: Project.self) })
+        
+        for i in 0 ..< projects.count {
+            let project = projects[i]
+            let ownerUid = project.ownerUid
+            let projectUser = try await FirestoreManager.shared.fetchUser(userId: ownerUid)
+            projects[i].user = projectUser
+        }
+        return projects
+    }
+    
+    func fetchUserProject(userId: String) async throws -> [Project] {
+        let snapshot = try await projectsCollectionPath.whereField("ownerUid", isEqualTo: userId).getDocuments()
+
+        return snapshot.documents.compactMap({ try? $0.data(as: Project.self) }).sorted(by: { $0.timeStamp.seconds > $1.timeStamp.seconds } )
+    }
+
+}
