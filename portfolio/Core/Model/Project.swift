a1e9acbf8926d64b484bef7a0a876fbbb9efbf16
Ozgun Aksoy <zgnaksoy@gmail.com>
2023-10-11 14:41:17 -0700

    add ispublic to model

--- portfolio/Core/Model/Project.swift
+++ portfolio/Core/Model/Project.swift
@@ -21,6 +21,7 @@ struct Project: Identifiable, Hashable, Codable {
     var timeStamp: Timestamp
     var videoUrl: String?
     var github: String?
+    var isPublic: Bool
     var user: DBUser?
 }
 
@@ -41,6 +42,7 @@ extension Project {
         timeStamp: Timestamp(),
         videoUrl: nil,
         github: "https://github.com/Ozgunak/InstagramSwiftUI",
+        isPublic: true,
         user: Optional(DBUser(id: "S8FRTEJwbpcqJcNvLLb7XFbp5pH2", username: "User 1", profileImageURL: Optional("https://firebasestorage.googleapis.com:443/v0/b/reservations-5ea6e.appspot.com/o/profileImages%2FF640F87E-3BB9-4174-BEE2-28481FDDFF62?alt=media&token=6e650eb0-e1e4-42bf-a661-b61e35c53d94"), fullName: Optional("Ozgun Aksoy"), title: Optional("IOS dev"), email: "1@2.com", joinDate: Optional(Timestamp()), followers: nil, following: nil, github: "github", projects: nil))
     )
     
@@ -59,7 +61,7 @@ extension Project {
                                                 ],
                                                 timeStamp: Timestamp(),
                                                 videoUrl: nil,
-                                                user: Optional(DBUser(id: "S8FRTEJwbpcqJcNvLLb7XFbp5pH2", username: "User 1", profileImageURL: Optional("https://firebasestorage.googleapis.com:443/v0/b/reservations-5ea6e.appspot.com/o/profileImages%2FF640F87E-3BB9-4174-BEE2-28481FDDFF62?alt=media&token=6e650eb0-e1e4-42bf-a661-b61e35c53d94"), fullName: Optional("Ozgun Aksoy"), title: Optional("IOS dev"), email: "1@2.com", joinDate: Optional(Timestamp()), followers: nil, following: nil, projects: nil))
+                                                isPublic: true, user: Optional(DBUser(id: "S8FRTEJwbpcqJcNvLLb7XFbp5pH2", username: "User 1", profileImageURL: Optional("https://firebasestorage.googleapis.com:443/v0/b/reservations-5ea6e.appspot.com/o/profileImages%2FF640F87E-3BB9-4174-BEE2-28481FDDFF62?alt=media&token=6e650eb0-e1e4-42bf-a661-b61e35c53d94"), fullName: Optional("Ozgun Aksoy"), title: Optional("IOS dev"), email: "1@2.com", joinDate: Optional(Timestamp()), followers: nil, following: nil, projects: nil))
                                             )
     ]
 }
