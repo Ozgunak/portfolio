8fa00db11438ba05e4f63d440025393722d8f7d3
Ozgun Aksoy <zgnaksoy@gmail.com>
2023-10-18 00:41:34 -0700

    add constants for firestore

---
+++ portfolio/Core/Components/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  portfolio
+//
+//  Created by özgün aksoy on 2023-10-17.
+//
+
+import Foundation
+
+struct K {
+    
+    struct FirestoreConstant {
+        static let messageList = "messageList"
+        static let chats = "chats"
+        static let toId = "toId"
+        static let timeStamp = "timeStamp"
+        static let messages = "messages"
+    }
+}
