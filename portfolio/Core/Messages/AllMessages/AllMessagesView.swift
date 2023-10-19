9d7fe8d660fd7f03fe9f2aae5b6c69903532d899
Ozgun Aksoy <zgnaksoy@gmail.com>
2023-10-18 22:13:20 -0700

    add sign up promotion view

--- portfolio/Core/Messages/AllMessages/AllMessagesView.swift
+++ portfolio/Core/Messages/AllMessages/AllMessagesView.swift
@@ -10,63 +10,65 @@ import SwiftUI
 struct AllMessagesView: View {
 //    @Environment(\.dismiss) var dismiss
     @StateObject var viewModel: AllMessagesViewModel = AllMessagesViewModel()
-    
+    @State private var showSignInView: Bool = false
+
     var body: some View {
         NavigationStack {
-            
             VStack {
-                ScrollView{
-                    LazyVStack {
-                        if viewModel.messages.isEmpty {
-                            ContentUnavailableView("No Messages Yet", systemImage: "message", description: Text("Start new conversation."))
-                        } else {
-                            ForEach(viewModel.messages) { message in
-                                NavigationLink {
-                                    if let messanger = viewModel.users.first(where: { $0.id == message.toId }) {
-                                        MessageView(messanger: messanger)
-                                            .navigationBarBackButtonHidden()
-                                    } else if let messanger = viewModel.users.first(where: { $0.id == message.ownerUid }) {
-                                        MessageView(messanger: messanger)
-                                            .navigationBarBackButtonHidden()
+                if viewModel.userSession?.isAnonymous == true {
+                    SignUpPromoteView()
+                } else {
+                    VStack {
+                        ScrollView{
+                            LazyVStack {
+                                if viewModel.messages.isEmpty {
+                                    ContentUnavailableView("No Messages Yet", systemImage: "message", description: Text("Start new conversation."))
+                                } else {
+                                    ForEach(viewModel.messages) { message in
+                                        NavigationLink {
+                                            if let messanger = viewModel.users.first(where: { $0.id == message.toId }) {
+                                                MessageView(messanger: messanger)
+                                                    .navigationBarBackButtonHidden()
+                                            } else if let messanger = viewModel.users.first(where: { $0.id == message.ownerUid }) {
+                                                MessageView(messanger: messanger)
+                                                    .navigationBarBackButtonHidden()
+                                            }
+                                        } label: {
+                                            MessageUserView(message: message)
+                                                .padding(.bottom, 4)
+                                        }
                                     }
-                                } label: {
-                                    MessageUserView(message: message)
-                                        .padding(.bottom, 4)
                                 }
-                                
-                                
+                            }
+                        }
+                        .task {
+                            try? await viewModel.fetchMessagedUsers()
+                            try? await viewModel.fetchUsers()
+                            try? await viewModel.fetchMessages()
+                        }
+                        .navigationTitle("Messages")
+                        .navigationBarTitleDisplayMode(.inline)
+                        .toolbar {
+                            ToolbarItem(placement: .topBarTrailing) {
+                                NavigationLink {
+                                    NewMessageView()
+                                        .navigationBarBackButtonHidden()
+                                } label: {
+                                    Image(systemName: "plus.bubble")
+                                }
                                 
                             }
                         }
                     }
                 }
-                .task {
-                    try? await viewModel.fetchMessagedUsers()
-                    try? await viewModel.fetchUsers()
-                    try? await viewModel.fetchMessages()
-                }
-                .navigationTitle("Messages")
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-//                    ToolbarItem(placement: .topBarLeading) {
-//                        Image(systemName: "chevron.left")
-//                            .imageScale(.large)
-//                            .onTapGesture {
-//                                dismiss()
-//                            }
-//                    }
-                    ToolbarItem(placement: .topBarTrailing) {
-                        NavigationLink {
-                            NewMessageView()
-                                .navigationBarBackButtonHidden()
-                        } label: {
-                            Image(systemName: "plus.bubble")
-                        }
-                        
-                    }
-                }
             }
+            .sheet(isPresented: $showSignInView, content: {
+//                NavigationStack {
+                    LoginView(showSignInView: $showSignInView)
+//                }
+            })
         }
+        
     }
 }
 
--- portfolio/Core/Messages/AllMessages/AllMessagesViewModel.swift
+++ portfolio/Core/Messages/AllMessages/AllMessagesViewModel.swift
@@ -7,38 +7,38 @@
 
 import Foundation
 import Firebase
+import Combine
 
 @MainActor
 class AllMessagesViewModel: ObservableObject {
     @Published var users = [DBUser]()
     @Published var messages: [Message] = []
     @Published var userIds = [String]()
+    @Published var userSession: User?
+    var subscriptions = Set<AnyCancellable>()
+    init() {
+        setupSubscribers()
+    }
+    
+    private func setupSubscribers() {
+        AuthenticationManager.shared.$userSession.sink { userSession in
+            self.userSession = userSession
+        }
+        .store(in: &subscriptions)
+    }
     
     func fetchMessagedUsers() async throws {
         guard let userId = Auth.auth().currentUser?.uid else { return }
-//        users = try await FirestoreManager.shared.fetchAllUsers()
-//        users.removeAll(where: { $0.id == Auth.auth().currentUser?.uid })
         userIds = try await MessageManager.shared.fetchMessagesUsers(userId: userId)
-//        var messageUsers: [DBUser] = []
-//        for i in 0..<messages.count {
-//            let message = messages[i]
-//            let user = try await FirestoreManager.shared.fetchUser(userId: message.toId)
-//            messageUsers.append(user)
-//        }
-//        users = messageUsers
-        print("userids after fetch \(userIds)")
     }
     
     func fetchUsers() async throws {
         users = try await MessageManager.shared.fetchUsers(userIdList: userIds)
-        print("users after fetch \(users)")
     }
     
     func fetchMessages() async throws {
         guard let userId = Auth.auth().currentUser?.uid else { return }
-
         messages = try await MessageManager.shared.fetchUsersWithMessages(userId: userId, otherUsers: users)
-        print("messages after fetch \(messages)")
     }
     
     
